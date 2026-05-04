184d1444554c0b9a50bfbb10895792924ec84a77
Juhi <snehabarthwal926@gmail.com>
2026-05-05 01:17:42 +0530

    requirement added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Juhi",
+    author_email="ji0164909@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
